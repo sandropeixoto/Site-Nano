ecdac61b174fdef5fdbc7d324ed8641ca39721b1
Sandro Peixoto <contato@sandropeixoto.com.br>
2026-04-24 06:31:37 -0300

    feat: configure root-level deployment with SPA redirect support for custom domain

--- src/main.tsx
+++ src/main.tsx
@@ -13,7 +13,7 @@ import "@fontsource/inter/600.css";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
